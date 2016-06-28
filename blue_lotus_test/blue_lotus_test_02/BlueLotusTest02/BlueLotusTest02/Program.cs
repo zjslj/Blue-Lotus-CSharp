be479b065756877ffbf75ce0cfa66af5d9724962
Stone Shi <SHILIJUN612@pingan.com.cn>
2016-06-28 17:50:43 +0800

    commit

---
+++ blue_lotus_test/blue_lotus_test_02/BlueLotusTest02/BlueLotusTest02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueLotusTest02
+{
+    class Program
+    {
+        #region Program
+        static void Main(string[] args)
+        {
+            #region Main() method
+            Console.WriteLine("BlueLotusTest02");
+            int i = 100;
+            string str = "test";
+            Test();
+            Console.ReadKey();
+            #endregion
+        }
+        private static void Test()
+        {
+            Console.WriteLine("CodeLens");
+        }
+        
+        #endregion 
+    }
+}
